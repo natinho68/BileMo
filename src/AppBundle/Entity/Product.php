97c8265454db076431b1e6a22e6602fa24833dcd
natinho68 <mandemons1@hotmail.com>
2017-10-24 11:26:42 +0200

    generate entities, configure fosuserbundle

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal", precision=2, scale=2)
+     */
+    private $price;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity_available", type="integer")
+     */
+    private $quantityAvailable;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="os", type="string", length=255)
+     */
+    private $os;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="color", type="string", length=255)
+     */
+    private $color;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="technical_sheet", type="text")
+     */
+    private $technicalSheet;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="brand", type="string", length=255)
+     */
+    private $brand;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="customer_reviews", type="text")
+     */
+    private $customerReviews;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set quantityAvailable
+     *
+     * @param integer $quantityAvailable
+     *
+     * @return Product
+     */
+    public function setQuantityAvailable($quantityAvailable)
+    {
+        $this->quantityAvailable = $quantityAvailable;
+
+        return $this;
+    }
+
+    /**
+     * Get quantityAvailable
+     *
+     * @return int
+     */
+    public function getQuantityAvailable()
+    {
+        return $this->quantityAvailable;
+    }
+
+    /**
+     * Set os
+     *
+     * @param string $os
+     *
+     * @return Product
+     */
+    public function setOs($os)
+    {
+        $this->os = $os;
+
+        return $this;
+    }
+
+    /**
+     * Get os
+     *
+     * @return string
+     */
+    public function getOs()
+    {
+        return $this->os;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     *
+     * @return Product
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set technicalSheet
+     *
+     * @param string $technicalSheet
+     *
+     * @return Product
+     */
+    public function setTechnicalSheet($technicalSheet)
+    {
+        $this->technicalSheet = $technicalSheet;
+
+        return $this;
+    }
+
+    /**
+     * Get technicalSheet
+     *
+     * @return string
+     */
+    public function getTechnicalSheet()
+    {
+        return $this->technicalSheet;
+    }
+
+    /**
+     * Set brand
+     *
+     * @param string $brand
+     *
+     * @return Product
+     */
+    public function setBrand($brand)
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    /**
+     * Get brand
+     *
+     * @return string
+     */
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * Set customerReviews
+     *
+     * @param string $customerReviews
+     *
+     * @return Product
+     */
+    public function setCustomerReviews($customerReviews)
+    {
+        $this->customer_reviews = $customerReviews;
+
+        return $this;
+    }
+
+    /**
+     * Get customerReviews
+     *
+     * @return string
+     */
+    public function getCustomerReviews()
+    {
+        return $this->customerReviews;
+    }
+}
