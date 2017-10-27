d2c9c984e702a6a691a8ad9e60025f4bbcc3a0f4
natinho68 <mandemons1@hotmail.com>
2017-10-27 12:34:41 +0200

    users cannot access responses from api without having the access token from front-end-app

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -15,6 +15,28 @@ use AppBundle\Exception\ResourceValidationException;
 class UserController extends FOSRestController
 {
 
+
+    public function authentification()
+    {
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', 'https://graph.facebook.com/me?fields=id,name', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization'     => 'Bearer EAAB58F0ToY8BAG3kHzNJz0VA6poquLKkYIXZAXRIqxy3bZAZAZCLa3AUrk0oEjFZAcxuXLfc2eGsyHZAq55SolkhiRYZC6OPNWo3TZBmFjEI4XRV9FbxnKSoXceddQDoesCbvq6pxnbVqlSznmdZBscQoP5B63vjv7Phbq8dy2nyHSwZDZD'
+            ]
+        ]);
+        $ok = $res->getStatusCode();
+        if($ok != 200){
+            echo $res->getReasonPhrase();
+        } else {
+            return true;
+        }
+
+// 200
+// 'application/json; charset=utf8'
+// '{"id": 1420053, "name": "guzzle", ...}'
+    }
+
     /**
      * @Rest\Get(
      *     path = "/users/{id}",
@@ -70,6 +92,7 @@ class UserController extends FOSRestController
      */
     public function listAction()
     {
+        $this->authentification();
         $users = $this->getDoctrine()->getRepository('AppBundle\Entity\User')->findAll();
         return $users;
     }
