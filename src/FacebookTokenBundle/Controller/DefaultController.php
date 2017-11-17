ce6327489d95dc65de3f3a91211a3470b8234566
natinho68 <mandemons1@hotmail.com>
2017-11-17 15:59:44 +0100

    adding @method for homepage

--- src/FacebookTokenBundle/Controller/DefaultController.php
+++ src/FacebookTokenBundle/Controller/DefaultController.php
@@ -4,12 +4,14 @@ namespace FacebookTokenBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Method("GET")
      */
     public function indexAction(Request $request)
     {
